b290dbd4a54df2be4078deb05ef86d0a84921f73
sesshu <ne300153@senshu-u.jp>
2021-03-18 17:44:22 +0900

    deviseの設定の完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => 'hiro58889@gmail.com',
+    :password => 'svzrfqejsmklzonr',
+    :authentication => 'login'
+  }
 end
