3a945d47fbfc31f3460ba31977cbb41c34cd350c
sesshu <ne300153@senshu-u.jp>
2021-03-19 19:26:54 +0900

    ユーザー一覧とユーザー詳細の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/index'
+
+  #get 'users/show'
+  #get 'users/index'
+  resources :users, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "サンプル",
+  email: "example@gmail.com",
+  password: "foobar",
+  confirmed_at: Time.now)
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Gimei.first.katakana
+  email = "example-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name:  name,
+    email: email,
+    password: password,
+    confirmed_at: Time.now)
+end
