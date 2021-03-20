b12df1acc63de8d500c26c6ac59f7cde7f1d04f4
sesshu <ne300153@senshu-u.jp>
2021-03-20 22:09:55 +0900

    新規投稿と投稿削除の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :correct_user,   only: :destroy
+
   def index
     @posts = Post.all.page(params[:page])
   end
@@ -6,4 +9,34 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "post created!"
+      redirect_to root_url
+    else
+      render 'posts/new'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:impression)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 end
