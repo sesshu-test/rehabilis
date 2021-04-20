4e6aca218c9e2bbabea34c88b0fcd6c7bef2d850
sesshu <ne300153@senshu-u.jp>
2021-04-20 13:56:09 +0900

    DMのajax化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room
+  before_action :get_all_messages
   
   def create
     message = Message.new(message_params)
     message.user_id = current_user.id
     if message.save
       message.create_notification_message!(current_user)
-      redirect_to room_path(message.room)
+      respond_to do |format|
+        #format.html { redirect_back(fallback_location: root_path) }
+        format.js
+      end
     else
       redirect_back(fallback_location: root_path)
     end
@@ -15,12 +20,23 @@ class MessagesController < ApplicationController
   def destroy
     message = Message.find(params[:id])
     message.destroy
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      #format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   private
 
     def message_params
       params.require(:message).permit(:room_id, :content)
+    end
+
+    def set_room
+      @room = Room.find(params[:message][:room_id])
+    end
+
+    def get_all_messages
+      @messages = @room.messages.includes(:user)
     end
 end
