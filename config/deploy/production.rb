d29f94369232390467c5ed0c97dcbb78f2c3ca73
sesshu <ne300153@senshu-u.jp>
2021-07-04 15:55:54 +0900

    name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '54.176.33.7', user: 'sesshu', roles: %w{app db web} 
+server '54.176.33.7', user: 'sesshu@ip-10-0-0-241', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
 set :ssh_options, keys: '~/.ssh/rehabilis_aws_rsa'
