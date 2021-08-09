cb9b94f1075ca91c41db3099cbf7fed52ec6e37a
sesshu <ne300153@senshu-u.jp>
2021-08-09 11:54:15 +0900

    メッセージとコメントの削除のテスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,25 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe 'コメント投稿', type: :system do
-  let(:post) { create(:post) }
-  let(:comment) { create(:comment) }
+RSpec.describe 'コメント', type: :system do
 
   # ログインしていないユーザーは、System/postsにてコメント投稿フォームが表示されないことを確認済み
 
-  it 'ログインしたユーザーはツイート詳細ページでコメントを投稿できる' do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:comment) { create(:comment) }
+  it 'ログインしたユーザーはツイート詳細ページでコメントを投稿し、それをできる' do
     # ログインする
-    signIn(post.user)
+    signIn(user)
     # 投稿詳細ページに遷移する
     visit post_path(post)
     # フォームに情報を入力する
     fill_in 'comment_content', with: comment.content
-    # コメントを送信すると、Commentモデルのカウントが1上がる
+    # コメントを送信するとページにコメントが表示され、Commentモデルのカウントが1上がる
     expect  do
-      find('input[name="commit"]').click
-      sleep 1
+      find('input[value="Comment"]').click
+      expect(page).to have_content(comment.content)
     end.to change {Comment.count}.by(1)
-    # コメント内容が表示されている
-    visit post_path(post)
-    expect(page).to have_content(comment.content)
+    # コメントを削除すると、Commentモデルのカウントが1下がる
+    expect do
+      accept_alert do
+        find_link('削除').click
+      end
+      sleep 1
+    end.to change { Comment.count }.by(-1)
+    # 投稿詳細のまま
+    expect(current_path).to eq post_path(post)
   end
+  it 'ログインしたユーザーは他人のコメントは削除できない' do
+    # ログインする
+    signIn(user)
+    # 投稿詳細ページに遷移する
+    visit post_path(comment.post)
+    # コメントの削除リンクがない
+    expect(page).to have_no_link '削除'
+  end
+  
 end
--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -1,45 +1,100 @@
 require 'rails_helper'
 
-RSpec.describe 'メッセージ送信', type: :system do
+RSpec.describe 'メッセージ', type: :system do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
+  let(:message) { create(:message) }
 
-  it 'ログインしたユーザーは他人にメッセージを送信でき、受信側は通知一覧とDM一覧から確認できる' do
-    test_message = 'テストメッセージ'
-    # ログインする
-    signIn(user)
-    # メッセージを送る相手のユーザ詳細ページに遷移する
-    visit user_path(other_user)
-    # ボタンを押してチャットルームに遷移する
-    click_button 'チャットを始める'
-    expect(page).to have_field 'メッセージを入力して下さい'
-    # メッセージを送信するとチャットルーム内に表示され、Messageモデルのカウントが１上がる
-    fill_in 'メッセージを入力して下さい', with: test_message
-    expect  do
-      click_button '送信'
+  describe 'メッセージ送信' do
+    it 'ログインしたユーザーは他人にメッセージを送信でき、受信側は通知一覧とDM一覧から確認できる' do
+      test_message = 'テストメッセージ'
+      # ログインする
+      signIn(user)
+      # メッセージを送る相手のユーザ詳細ページに遷移する
+      visit user_path(other_user)
+      # ボタンを押してチャットルームに遷移する
+      click_button 'チャットを始める'
+      expect(page).to have_field 'メッセージを入力して下さい'
+      # メッセージを送信するとチャットルーム内に表示され、Messageモデルのカウントが１上がる
+      fill_in 'メッセージを入力して下さい', with: test_message
+      expect  do
+        click_button '送信'
+        expect(page).to have_content test_message
+      end.to change {Message.count}.by(1)
+      # DM一覧にて送信したメッセージが表示される
+      visit rooms_path
       expect(page).to have_content test_message
-    end.to change {Message.count}.by(1)
-    # DM一覧にて送信したメッセージが表示される
-    visit rooms_path
-    expect(page).to have_content test_message
-    # ログアウトする
-    find_link('ログアウト').click
-    expect(current_path).to eq root_path
-    # 受信側のユーザでログインする
-    signIn(other_user)
-    # 通知一覧に受信したメッセージが表示される
-    visit notifications_path
-    expect(page).to have_content test_message
-    # DM一覧で受信したメッセージのリンクを踏むと、チャットールームに遷移する
-    visit rooms_path
-    find_link(test_message).click
-    expect(page).to have_content(test_message)
-    expect(page).to have_field 'メッセージを入力して下さい'
+      # ログアウトする
+      find_link('ログアウト').click
+      expect(current_path).to eq root_path
+      # 受信側のユーザでログインする
+      signIn(other_user)
+      # 通知一覧に受信したメッセージが表示される
+      visit notifications_path
+      expect(page).to have_content test_message
+      # DM一覧で受信したメッセージのリンクを踏むと、チャットールームに遷移する
+      visit rooms_path
+      find_link(test_message).click
+      expect(page).to have_content(test_message)
+      expect(page).to have_field 'メッセージを入力して下さい'
+    end
+    it 'ログインしていないユーザは他人にメッセージを送信できない' do
+      # メッセージを送る相手のユーザ詳細ページに遷移する
+      visit user_path(other_user)
+      # チャットルームへのリンクがない
+      expect(page).to have_no_content('チャットを始める')
+    end
   end
-  it 'ログインしていないユーザは他人にメッセージを送信できない' do
-    # メッセージを送る相手のユーザ詳細ページに遷移する
-    visit user_path(other_user)
-    # チャットルームへのリンクがない
-    expect(page).to have_no_content('チャットを始める')
+
+  describe 'メッセージ削除' do
+    it '自分のメッセージは削除できる' do
+      test_message = 'テストメッセージ'
+      # ログインする
+      signIn(user)
+      # メッセージを送る相手のユーザ詳細ページに遷移する
+      visit user_path(other_user)
+      # ボタンを押してチャットルームに遷移する
+      click_button 'チャットを始める'
+      expect(page).to have_field 'メッセージを入力して下さい'
+      # メッセージを送信するとチャットルーム内に表示され、Messageモデルのカウントが１上がる
+      fill_in 'メッセージを入力して下さい', with: test_message
+      expect  do
+        click_button '送信'
+        expect(page).to have_content test_message
+      end.to change {Message.count}.by(1)
+      # コメントを削除すると、Commentモデルのカウントが1下がる
+      expect do
+        accept_alert do
+          click_button '削除'
+        end
+        sleep 1
+      end.to change { Message.count }.by(-1)
+    end
+    it '他人のメッセージは削除できない' do
+      test_message = 'テストメッセージ'
+      # ログインする
+      signIn(other_user)
+      # メッセージを送る相手のユーザ詳細ページに遷移する
+      visit user_path(user)
+      # ボタンを押してチャットルームに遷移する
+      click_button 'チャットを始める'
+      expect(page).to have_field 'メッセージを入力して下さい'
+      # メッセージを送信するとチャットルーム内に表示され、Messageモデルのカウントが１上がる
+      fill_in 'メッセージを入力して下さい', with: test_message
+      expect  do
+        click_button '送信'
+        expect(page).to have_content test_message
+      end.to change {Message.count}.by(1)
+      # ログアウトする
+      find_link('ログアウト').click
+      expect(current_path).to eq root_path
+      # 受信側のユーザでログインする
+      signIn(user)
+      # DM一覧で受信したメッセージのリンクを踏むと、チャットールームに遷移する
+      visit rooms_path
+      find_link(test_message).click
+      expect(page).to have_no_button '削除'
+    end
   end
+  
 end
