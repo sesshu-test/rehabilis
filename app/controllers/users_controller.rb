27b062414e7e1e69cb0749c682f296e26eebebdb
sesshu <ne300153@senshu-u.jp>
2021-09-23 09:28:27 +0900

    ゲストログイン機能の完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,16 @@ class UsersController < ApplicationController
     render 'show_follow'
   end
 
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.confirmed_at = Time.now
+      user.name = "ゲスト"
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   def graph
     @count_or_time = params[:count_or_time]
     get_rehabilitations_data(@count_or_time)
